a80e1005146c0503bfdb88d8a39f7299d81866a9
JuSugang <tnrkd7679@naver.com>
2018-01-24 19:54:36 +0900

    save 완성중

--- Assets/scripts/Manager.cs
+++ Assets/scripts/Manager.cs
@@ -1,33 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using Vuforia;
 
 public class Manager : MonoBehaviour
 {
     public GameObject ARCamera;
+    public GameObject wall;
     private SmartTerrainTracker mTracker;
     private ReconstructionBehaviour mReconstructionBehaviour;
     private PropBehaviour mPropBehaviour;
+    
     // Use this for initialization
     void Start () {
         mTracker = TrackerManager.Instance.GetTracker<SmartTerrainTracker>();
         mReconstructionBehaviour = (ReconstructionBehaviour)FindObjectOfType(typeof(ReconstructionBehaviour));
-        Debug.Log("h1 " + mTracker);
-        Debug.Log("h2 " + mReconstructionBehaviour);
     }
 
     public void saveMesh()
     {
-        mTracker = TrackerManager.Instance.GetTracker<SmartTerrainTracker>();
-        mReconstructionBehaviour = (ReconstructionBehaviour)FindObjectOfType(typeof(ReconstructionBehaviour));
-        Debug.Log("h1 " + mTracker);
-        Debug.Log("h2 " + mReconstructionBehaviour);
+        if(mTracker==null || mReconstructionBehaviour == null) { 
+            mTracker = TrackerManager.Instance.GetTracker<SmartTerrainTracker>();
+            mReconstructionBehaviour = (ReconstructionBehaviour)FindObjectOfType(typeof(ReconstructionBehaviour));
+        }
+        IEnumerable<Prop> propIEnu= mReconstructionBehaviour.GetActiveProps();
+        IEnumerator<Prop> e = propIEnu.GetEnumerator();
+        int count = 0;
+        while (e.MoveNext())
+        {
+            count++;
+
+            var mf = wall.GetComponent<MeshFilter>();
+            var mesh=mf.sharedMesh;
+            mesh.vertices= e.Current.GetMesh().vertices;
+            mesh.triangles = e.Current.GetMesh().triangles;
+            
+            //Object tempMesh = e.Current.GetMesh();
+            //Vector3[] temp = e.Current.GetMesh().vertices;
+            //int[] b =e.Current.GetMesh().triangles;
+            //Debug.Log(e.Current.GetMesh().vertexCount);
+            
+            //foreach(Vector3 a in temp)
+            //{
+            //    Debug.Log("coord= "+a);
+            //}
+            //foreach (int a in b)
+            //{
+            //    Debug.Log("tri= " + a);
+            //}
+            //SaveObjectToFile(tempMesh, "Assets/data" + count+".obj");
+
+        }
+        Debug.Log("count= " + count);
+    }
+    public static void SaveObjectToFile(Object obj, string fileName)
+    {
+        AssetDatabase.CreateAsset(obj, fileName);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
     public void restart()
     {
-        mTracker = TrackerManager.Instance.GetTracker<SmartTerrainTracker>();
-        mReconstructionBehaviour = (ReconstructionBehaviour)FindObjectOfType(typeof(ReconstructionBehaviour));
+        if (mTracker == null || mReconstructionBehaviour == null)
+        {
+            mTracker = TrackerManager.Instance.GetTracker<SmartTerrainTracker>();
+            mReconstructionBehaviour = (ReconstructionBehaviour)FindObjectOfType(typeof(ReconstructionBehaviour));
+        }
         if ((mReconstructionBehaviour != null) && (mReconstructionBehaviour.Reconstruction != null))
         {
             bool trackerWasActive = mTracker.IsActive;
---
+++ Assets/scripts/meshtest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshtest : MonoBehaviour {
+    public GameObject before;
+    public GameObject after;
+
+
+    // Use this for initialization
+    void Start () {
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Mesh mesh = before.GetComponent<MeshFilter>().sharedMesh;
+        Vector3[] vertices = mesh.vertices;
+        int i = 0;
+ 
+        //while (i < vertices.Length)
+        //{
+        //    vertices[i] = before.GetComponent<MeshFilter>().sharedMesh.vertices[i];
+        //    i++;
+        //}
+        vertices = after.GetComponent<MeshFilter>().sharedMesh.vertices;
+        //vertices = a.sharedMesh.vertices;
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+    }
+}
